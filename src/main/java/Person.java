f9c0e7317cacce8e2a997c2910b9dc6d5d94383e
David <davidsamcun@gmail.com>
2022-04-13 16:13:56 -0400

    save

---
+++ src/main/java/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+
+    String firstName;
+    String lastName;
+    int id;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
