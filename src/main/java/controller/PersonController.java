82dfbbb46a08d7e9a4fecb423eda2d2f72baa985
David <davidsamcun@gmail.com>
2022-04-13 16:32:50 -0400

    established part 1

---
+++ src/main/java/controller/PersonController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import model.Person;
+
+import java.util.List;
+
+public class PersonController {
+
+    public Person createPerson(Person p){
+
+        return null;
+    }
+
+    public Person getPerson(int id){
+
+        return null;
+    }
+
+    public List<Person> getPersonList(){
+
+        return null;
+    }
+
+    public Person updatePerson(Person p){
+
+        return null;
+    }
+
+    public void deletePerson(int id){
+
+
+    }
+}
